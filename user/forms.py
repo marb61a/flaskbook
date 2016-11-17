29eca77a69cdcff64872549235431f3cec85b5d2
martin byrne <marb61a@gmail.com>
2016-11-17 15:03:43 +0000

    updated form

--- user/forms.py
+++ user/forms.py
@@ -8,3 +8,26 @@ from user.models import User
 class RegisterForm(Form):
     first_name = StringField('First Name', [validators.Required()])
     last_name = StringField('Last Name', [validators.Required()])
+    email = EmailField('Email address', [
+        validators.DataRequired(),
+        validators.Email()
+        ]
+    )
+    username = StringField('Username', [
+        validators.Required(),
+        validators.length(min=4, max=25)
+        ])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.length(min=4, max=80)
+        ])
+    confirm = PasswordField('Repeat Password')
+    
+    def validate_username(form, field):
+        if User.objects.filter(username=field.data).first():
+            raise ValidationError("Username already exists")
+    
+    def validate_email(form, field):
+        if User.objects.filter(email=field.data).first():
+            raise ValidationError("Email is already in use")
--- user/views.py
+++ user/views.py
@@ -9,3 +9,21 @@ user_app = Blueprint('user_app', __name__)
 @user_app.route('/login')
 def login():
     return "User login"
+
+@user_app.route('/register', methods=('GET','POST'))
+def register():
+    form = RegisterForm
+    if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(form.password.data, salt)
+        user = User(
+            username=form.username.data,
+            password=hashed_password,
+            email=form.email.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data
+            )
+        user.save()
+        return "User Registered"
+    return render_template('user/register.html', form=form)
+        
