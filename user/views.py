34f685dd5e84874c631dee4d4a7cfbf3a1c2f1ff
martin byrne <marb61a@gmail.com>
2016-11-29 14:23:58 +0000

    added edit route to user views

--- user/views.py
+++ user/views.py
@@ -69,3 +69,31 @@ def profile(username):
         return render_template('user/profile.html', user=user, edit_profile=edit_profile)
     else:
         abort(404)
+        
+
+@user_app.route('/edit', methods=('GET','POST'))
+def edit():
+    error = None
+    message = None
+    user = User.objects.filter(username=session.get('username')).first()
+    if user:
+        form = EditForm(obj=user)
+        if form.validate_on_submit():
+            if user.username != form.username.data:
+                if User.objects.filter(username=form.username.data.lower()).first():
+                    error = "Username already exists"
+                else:
+                    session['username'] = form.username.data.lower()
+                    form.username.data = form.username.data.lower()
+            if user.email != form.email.data:
+                if User.objects.filter(email=form.email.data.lower()).first():
+                    error = "Email already exists"
+                else:
+                    form.email.data = form.email.data.lower()
+            if not error:
+                form.populate_obj(user)
+                user.save()
+                message = "Profile updated"
+    return render_template("user/edit.html", form=form, error=error, message=message)        
+    else:
+        abort(404)
