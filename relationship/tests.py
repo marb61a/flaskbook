dbb7b758525e314581137d564a5cfe633606393e
martin byrne <marb61a@gmail.com>
2017-01-16 10:27:24 +0000

    updated relationship test file

--- relationship/tests.py
+++ relationship/tests.py
@@ -43,3 +43,35 @@ class RelationshipTest(unittest.TestCase):
             password="abcd1234",
             confirm="abcd1234"
             )
+            
+    def test_friends_operations(self):
+        # Register users
+        rv = self.app.post('/register', data=self.user1_dict(), follow_redirects=True)
+        assert User.objects.filter(username=self.user1_dict()['username']).count() == 1
+        rv = self.app.post('/register', data=self.user2_dict(), follow_redirects=True)
+        assert User.objects.filter(username=self.user2_dict()['username']).count() == 1
+        
+        # Login the first user
+        rv = self.app.post('/login', data=dict(
+            username=self.user1_dict()['username'],
+            password=self.user1_dict()['password']
+            ))
+        
+        # Add the second user as a friend
+        rv = self.app.get('/add_friend/' + self.user2_dict()['username'], follow_redirects=True)
+        assert "relationship-friends-requested" in str(rv.data)
+        
+        # Check that only one record exists at this point
+        relcount = Relationship.objects.count()
+        assert relcount == 1
+        
+        # Login the second user
+        rv = self.app.post('/login', data=dict(
+            username=self.user2_dict()['username'],
+            password=self.user2_dict()['password']
+            ))
+        
+        # Check that a friend request is pending
+        rv = self.app.get('/' + self.user1_dict()['username'])
+        assert "relationship-reverse-friends-requested" in str(rv.data)
+        
