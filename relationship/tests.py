57dc55b10107426c7aa56afb9129eb7bce0bc6eb
martin byrne <marb61a@gmail.com>
2017-01-18 10:06:03 +0000

    updated relationship tests

--- relationship/tests.py
+++ relationship/tests.py
@@ -75,3 +75,69 @@ class RelationshipTest(unittest.TestCase):
         rv = self.app.get('/' + self.user1_dict()['username'])
         assert "relationship-reverse-friends-requested" in str(rv.data)
         
+        # Confirm first user as a friend
+        rv = self.app.get('/add_friend/' + self.user1_dict()['username'], follow_redirects=True)
+        assert "relationship-friends" in str(rv.data)
+        
+        # Check that two records exist at this point
+        relcount = Relationship.objects.count()
+        assert relcount == 2
+        
+        # User2 unfriends User1
+        rv = self.app.get('/remove_friend/' + self.user1_dict()['username'], follow_redirects=True)
+        assert "relationship-add-friend" in str(rv.data)
+        
+        # Check that no records exist at this point
+        relcount = Relationship.objects.count()
+        assert relcount == 0
+        
+        # Login the first user
+        rv = self.app.post('/login', data=dict(
+            username=self.user1_dict()['username'],
+            password=self.user1_dict()['password']
+            ))
+        
+        # Check no longer friends
+        rv = self.app.get('/' + self.user2_dict()['username'])
+        assert "relationship-add-friend" in str(rv.data)
+    
+    def test_block_operations(self):
+        # Register users
+        rv = self.app.post('/register', data=self.user1_dict(), follow_redirects=True)
+        assert User.objects.filter(username=self.user1_dict()['username']).count() == 1
+        rv = self.app.post('/register', data=self.user2_dict(), follow_redirects=True)
+        assert User.objects.filter(username=self.user2_dict()['username']).count() == 1
+        
+        # Login the first user
+        rv = self.app.post('/block/', data=dict(
+            username=self.user1_dict()['username'],
+            password=self.user1_dict()['password']
+            ))
+        
+        # User1 blocks User2
+        rv = self.app.get('/remove_friend/' + self.user2_dict()['username'], follow_redirects=True)
+        assert "relationship-blocked" in str(rv.data)
+        
+        # Login the second user
+        rv = self.app.post('/login', data=dict(
+            username=self.user2_dict()['username'],
+            password=self.user2_dict()['password']
+            ))
+        
+        # Check user1 profile reflects being blocked
+        rv = self.app.get('/' + self.user1_dict()['username'])
+        assert "relationship-reverse-blocked" in str(rv.data)
+        
+        # Try become friends with User1
+        rv = self.app.get('/add_friend/' + self.user1_dict()['username'], follow_redirects=True)
+        assert "relationship-reverse-blocked" in str(rv.data)
+        
+        # Login the first user
+        rv = self.app.post('/block/', data=dict(
+            username=self.user1_dict()['username'],
+            password=self.user1_dict()['password']
+            ))
+        
+        # User1 unblocks User2
+        rv = self.app.get('/unblock/' + self.user2_dict()['username'], follow_redirects=True)
+        assert "relationship-add-friend" in str(rv.data)
